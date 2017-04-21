95a94e279dace604f602441e237a88cbb7c76c08
Su Gi Chandran <chandran.su.gi@gmail.com>
2017-04-20 21:14:15 -0700

    Added UI elements

--- client/src/views/App.js
+++ client/src/views/App.js
@@ -4,12 +4,27 @@ export default class App extends React.Component {
 
   render() {
     return <div className="list-container">
+      {this.searchBar()}
       {this.listArea()}
+      {this.buttonsArea()}
     </div>;
+  }
+
+  searchBar(){
+    return <input type="text" placeholder="Search" />
   }
 
   listArea(){
     return <div className="list-area">Test</div>;
+  }
+
+  buttonsArea(){
+    return <span>
+      <button>Open All</button>
+      <button>Close All</button>
+      <button>Toggle All</button>
+      <button>Add</button>
+    </span>
   }
 
 }
